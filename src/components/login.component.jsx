d3cd20f827302252a50849d12dbc5757afe792a5
Amnok <ankitsingh80741@gmail.com>
2020-02-18 02:24:27 +0530

    PC [feature] added backgrounds

--- src/components/login.component.jsx
+++ src/components/login.component.jsx
@@ -2,17 +2,31 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import url from '../assets/images/banner.jpg';
 
 
 const useStyles = makeStyles({
     root: {
         padding: '20px',
         display: 'flex',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        backgroundImage: `url(${url})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        width: '100vw',
+        height: '100vh'
     },
-    card: {
-        width: '675px',
-        padding: '20px'
+    paper: {
+        width: '575px',
+        height: '500px',
+        padding: '40px',
+        marginTop: '150px',
+        borderRadius: '10px'
+    },
+    button: {
+        marginTop: '60px'
     }
 });
 
@@ -20,25 +34,27 @@ export const Login = () => {
     const classes = useStyles();
     return (
         <div className={classes.root}>
-            <Form className={classes.card}>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                </Form.Text>
-                </Form.Group>
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Remember me" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-            </Button>
-            </Form>
+            <Paper elevation={0} className={classes.paper}>
+                <Form >
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Remember me" />
+                    </Form.Group>
+                    <Button className={classes.button} variant="primary" type="submit" size="lg" block>
+                        Submit
+                    </Button>
+                </Form>
+            </Paper>
         </div>
     )
 }
--- src/components/routes.js
+++ src/components/routes.js
@@ -1,11 +1,21 @@
 export default () => {
-    return [{
-        text: 'Posts',
-        link: '/'
-    },
-    {
-        text: 'Create Post',
-        link: '/create-post'
-    }]
+    return [
+        {
+            text: 'Posts',
+            link: '/'
+        },
+        {
+            text: 'Create Post',
+            link: '/create-post'
+        },
+        {
+            text: 'Create Account',
+            link: '/signup'
+        },
+        {
+            text: 'Logout',
+            link: '/login'
+        },
+    ]
 };
 
