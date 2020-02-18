afecdaeb3476774bfdbb08307c42c171a2d2f8c1
Amnok <ankitsingh80741@gmail.com>
2020-02-19 01:21:51 +0530

    PC [fix] fixed createpost form

--- src/components/createPost.component.jsx
+++ src/components/createPost.component.jsx
@@ -1,54 +1,99 @@
 import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import { Link } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import Container from '@material-ui/core/Container';
+import url from '../assets/images/banner.jpg';
 
-const useStyles = makeStyles({
-    root: {
+const useStyles = makeStyles(theme => ({
+    container: {
         padding: '20px',
         display: 'flex',
-        justifyContent :'center'
+        justifyContent: 'center',
+        width: '100vw',
+        height: '100vh',
+        // backgroundImage: `url(${url})`,
+        // backgroundPosition: 'center',
+        // backgroundSize: 'cover',
+        // backgroundRepeat: 'no-repeat',
     },
-    card: {
-        width: '675px',
-        padding: '20px'
-    }
-});
-
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
 
 export const CreatePost = () => {
     const classes = useStyles();
-
-    const [values, setValues] = React.useState({
-        amount: '',
-        password: '',
-        weight: '',
-        weightRange: '',
-        showPassword: false,
-    });
-
-    const handleChange = prop => event => {
-        setValues({ ...values, [prop]: event.target.value });
-    };
-
     return (
-        <div className={classes.root}>
-            <Form className={classes.card}>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Enter Title of your post</Form.Label>
-                    <Form.Control type="text" placeholder="Enter Title" />
-                    <Form.Text className="text-muted">
-                        Title of your post will be used for searching purpose.
-                    </Form.Text>
-                </Form.Group>
-                <Form.Group controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>Enter Details of your post</Form.Label>
-                    <Form.Control as="textarea" rows="3" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Create Post
-                </Button>
-            </Form>
+        <div className={classes.container}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <div className={classes.paper}>
+                    {/* <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography> */}
+                    <form className={classes.form} noValidate>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Enter title"
+                            name="title"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="content"
+                            label="Enter Content"
+                            type="text"
+                            id="content"
+                            autoComplete="current-password"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="secondary"
+                            className={classes.submit}
+                        >
+                        Add Post
+                        </Button>
+                    </form>
+                </div>
+            </Container>
         </div>
-    )
-};
+    );
+}
