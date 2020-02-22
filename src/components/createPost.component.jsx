49d6ad80123fd0e4e7be6cbc6e2a7ae4ce192658
Amnok <ankitsingh80741@gmail.com>
2020-02-22 23:06:42 +0530

    pc [fix] create post screen fix

--- src/components/createPost.component.jsx
+++ src/components/createPost.component.jsx
@@ -1,101 +1,184 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import { Link } from 'react-router-dom';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
 import url from '../assets/images/banner.jpg';
 
 const useStyles = makeStyles(theme => ({
-    container: {
-        padding: '20px',
-        display: 'flex',
-        justifyContent: 'center',
-        width: '100vw',
-        height: '100vh',
-        // backgroundImage: `url(${url})`,
-        // backgroundPosition: 'center',
-        // backgroundSize: 'cover',
-        // backgroundRepeat: 'no-repeat',
-    },
-    paper: {
-        marginTop: theme.spacing(8),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
+  container: {
+    padding: '20px',
+    display: 'flex',
+    justifyContent: 'center',
+    width: '100vw',
+    height: '100vh'
+    // backgroundImage: `url(${url})`,
+    // backgroundPosition: 'center',
+    // backgroundSize: 'cover',
+    // backgroundRepeat: 'no-repeat',
+  },
+  textFieldMargin: {
+    marginRight: theme.spacing(5)
+  },
+  paper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  row: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    width: theme.spacing(30),
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2)
+  }
 }));
 
-export const CreatePost = (props) => {
-    console.log("filter CreatePost", props);
-    
-    const classes = useStyles();
-    return (
-        <div className={classes.container}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <div className={classes.paper}>
-                    {/* <Avatar className={classes.avatar}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Sign in
-                    </Typography> */}
-                    <form className={classes.form} noValidate>
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Enter title"
-                            name="title"
-                            autoComplete="email"
-                            autoFocus
-                        />
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="content"
-                            label="Enter Content"
-                            type="text"
-                            id="content"
-                            autoComplete="current-password"
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="secondary"
-                            className={classes.submit}
-                        >
-                        Add Post
-                        </Button>
-                    </form>
-                </div>
-            </Container>
+export const CreatePost = props => {
+  console.log('filter CreatePost', props);
+  const [age, setAge] = React.useState('');
+  const inputLabel = React.useRef(null);
+  const [labelWidth, setLabelWidth] = React.useState(0);
+  React.useEffect(() => {
+    setLabelWidth(inputLabel.current.offsetWidth);
+  }, []);
+  const classes = useStyles();
+  const handleChange = event => {
+    setAge(event.target.value);
+  };
+  return (
+    <div className={classes.container}>
+      <Container component="main" maxWidth="md">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <form className={classes.form} noValidate>
+            <div className={classes.row}>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                size="medium"
+                id="email"
+                label="Enter Item name"
+                name="name"
+                autoComplete="name"
+                autoFocus
+                className={classes.textFieldMargin}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="price"
+                label="Enter Price"
+                type="number"
+                id="content"
+                autoComplete="current-password"
+              />
+            </div>
+            <div className={classes.row}>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                size="medium"
+                id="email"
+                label="Enter Item name"
+                name="name"
+                autoComplete="name"
+                autoFocus
+                className={classes.textFieldMargin}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="content"
+                label="Enter Content"
+                type="text"
+                id="content"
+                autoComplete="current-password"
+              />
+            </div>
+            <div>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                size="medium"
+                id="email"
+                label="Enter Item Details ....."
+                name="name"
+                autoComplete="name"
+                autoFocus
+                className={classes.textFieldMargin}
+                rows="4"
+                multiline
+              />
+            </div>
+            <FormControl variant="outlined" className={classes.formControl}>
+              <InputLabel
+                ref={inputLabel}
+                id="demo-simple-select-outlined-label"
+              >
+                Category
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-outlined-label"
+                id="demo-simple-select-outlined"
+                value={age}
+                onChange={handleChange}
+                labelWidth={labelWidth}
+                // fullWidth
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="secondary"
+              className={classes.submit}
+            >
+              Add Product
+            </Button>
+          </form>
         </div>
-    );
-}
+      </Container>
+    </div>
+  );
+};
